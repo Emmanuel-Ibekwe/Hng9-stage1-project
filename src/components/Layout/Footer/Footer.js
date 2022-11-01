8890dbd2b78b84672fe72d08174d4af07920d8a6
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-01 03:27:46 +0100

    fix: Add some padding-bottom to SocialIcons div

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,20 @@
+import "./Footer.css";
+import IngressiveLogo from "../../../assets/ingressive-logo.png";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="first-column">
+        Zuri<span className="dot">.</span>Internship
+      </div>
+      <div className="middle-column">
+        <small>HNG Internship 9 Frontend Task</small>
+      </div>
+      <div className="ingressive__logo--div">
+        <img className="ingressive__logo" src={IngressiveLogo} />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import MainHeader from "./MainHeader/MainHeader";
 import "./Layout.css";
+import Footer from "./Footer/Footer";
 
 const Layout = (props) => {
   return (
     <div className="layout">
       <MainHeader />
       <main>{props.children}</main>
+      <Footer />
     </div>
   );
 };
