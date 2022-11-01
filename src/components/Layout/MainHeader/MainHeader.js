ed87df93a5cf703268d15aae2d70dc1af5f0c957
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-01 11:09:55 +0100

    feat: Add a copy-to-clipboard functionality to btn__share button

--- src/components/Layout/MainHeader/MainHeader.js
+++ src/components/Layout/MainHeader/MainHeader.js
@@ -1,12 +1,43 @@
-import { useState } from "react";
+import { useCallback, useEffect, useReducer, useState } from "react";
 import "./MainHeader.css";
 import profileImg from "../../../assets/profile__img.jpeg";
 import ProfileImgOverlay from "./ProfileImgOverlay";
 import MoreIcon from "../../Icons/MoreIcon";
 import ShareIcon from "../../Icons/ShareIcon";
 
+const linkCopiedReducer = (state, action) => {
+  if (action.type === "btn__share clicked") {
+    return {
+      linkCopied: true,
+      shareBtnClicks: state.shareBtnClicks + 1,
+    };
+  }
+
+  if (action.type === "reset tooltip") {
+    return {
+      linkCopied: false,
+      shareBtnClicks: state.shareBtnClicks,
+    };
+  }
+};
+
 const MainHeader = () => {
   const [imgOverlay, setImgOverlay] = useState(false);
+  const [displayShareBtn, setdisplayShareBtn] = useState(false);
+
+  // This usereducer and other related functions adds a copy-to-clipboard functionality to the btn__share buttton
+  const [linkCopiedState, dispatchAction] = useReducer(linkCopiedReducer, {
+    linkCopied: false,
+    shareBtnClicks: 0,
+  });
+
+  useEffect(() => {
+    if (linkCopiedState.shareBtnClicks > 0) {
+      setTimeout(() => {
+        dispatchAction({ type: "reset tooltip" });
+      }, 500);
+    }
+  }, [linkCopiedState.shareBtnClicks]);
 
   const mouseEnterHandler = () => {
     setImgOverlay(true);
@@ -14,6 +45,15 @@ const MainHeader = () => {
 
   const mouseLeaveHandler = () => {
     setImgOverlay(false);
+  };
+
+  const displayShareBtnHandler = () => {
+    setdisplayShareBtn(true);
+  };
+
+  const shareBtnOnClickHandler = () => {
+    navigator.clipboard.writeText(window.location.href);
+    dispatchAction({ type: "btn__share clicked" });
   };
 
   return (
@@ -30,11 +70,19 @@ const MainHeader = () => {
         <p id="twitter">Emmanuel</p>
         <p id="slack">Immanuel1️⃣</p>
       </div>
-      <button className="btn__share">
+      <button
+        className={`btn btn__share ${displayShareBtn ? "reposition" : ""}`}
+        onClick={shareBtnOnClickHandler}
+      >
         <ShareIcon className={"share-icon"} />
-        <span className="share-icon__tooltip">Share Link</span>
+        <span className="share-icon__tooltip">
+          {linkCopiedState.linkCopied ? "Link copied!" : "Share Link"}
+        </span>
       </button>
-      <button className="btn__more">
+      <button
+        className={`btn btn__more ${!displayShareBtn ? "" : "hidden"}`}
+        onClick={displayShareBtnHandler}
+      >
         <MoreIcon className={"more-icon"} />
       </button>
     </header>
