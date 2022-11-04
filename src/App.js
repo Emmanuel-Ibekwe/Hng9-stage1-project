fcdac2282ceeccfe733341cd7bd13b6327efebc4
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-04 20:46:43 +0100

    feat: Add Routes and Route components to App.js file to enable navigation between the Home page and the ContactForm.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
+import ContactForm from "./components/Contact/ContactForm";
 
 function App() {
   return (
     <Layout>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<ContactForm />} />
+      </Routes>
     </Layout>
   );
 }
