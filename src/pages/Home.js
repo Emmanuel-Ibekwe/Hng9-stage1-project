967d83528936a870dfd15bba0210a5b393cdb034
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-05 22:26:34 +0100

    chore: Add window.scrollTo() to counteract the effect of its animation when the Home page loads in order to display the success message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import MainHeader from "../components/Layout/MainHeader/MainHeader";
 import SocialIcons from "../components/Icons/SocialIcons";
 import LinkList from "../components/Links/LinkList";
 
 const Home = () => {
+  useEffect(() => {
+    // scrolls to top once page loads
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
+
   return (
     <Fragment>
       <MainHeader />
