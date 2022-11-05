f30e18e9ecada2a8214aee5007b6ad2a78d48ad7
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-05 17:04:44 +0100

    feat: Create the Contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import ContactForm from "../components/Contact/ContactForm";
+import Footer from "../components/Layout/Footer/Footer";
+
+const Contact = () => {
+  return (
+    <Fragment>
+      <ContactForm />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Contact;
